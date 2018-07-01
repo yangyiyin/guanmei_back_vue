6595459814c4d7bd588b0f8b6730f54e7192b434
yyy <178340042@qq.com>
2018-07-01 18:47:07 +0800

    123

--- src/page_biz/managerrecommend_template.vue
+++ src/page_biz/managerrecommend_template.vue
@@ -279,7 +279,7 @@
                 managerrecommend_tmp_info({id:this.id}).then(function(res){
                     if (res.code == this.$store.state.constant.status_success) {
                         this.activeName = 'first';
-                        this.type = res.data.type;
+                        this.type = parseInt(res.data.type);
                         this.title = res.data.title;
                         this.img = res.data.img;
                         this.tmp_data = res.data.tmp_data;
