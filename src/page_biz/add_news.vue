4318fc4a10eb26da06489e2ca89e3c796a946c5c
yyy <178340042@qq.com>
2018-08-07 17:54:10 +0800

    123

--- src/page_biz/add_news.vue
+++ src/page_biz/add_news.vue
@@ -22,12 +22,27 @@
                 <i v-else class="el-icon-plus avatar-uploader-icon"></i>
             </el-upload>
 
+            <p class="search_item" style="font-size: 12px">内容编辑:<a href="javascript:;" v-if="editor == 1" @click="editor=2">< 使用代码编辑 > </a><a href="javascript:;" v-if="editor == 2" @click="editor=1">[使用可视化编辑]</a></p>
+            <div class="search_item" v-if="editor == 1">
 
-            <div class="search_item">
-                <quill-editor :content="content" :options = "editorOption" @change="onEditorChange($event)"></quill-editor>
+                <quill-editor ref="myQuillEditor" :content="content" :options = "editorOption" @change="onEditorChange($event)"></quill-editor>
+
+                <el-upload style="display: none"
+                        class="avatar-uploader"
+                           ref="upload"
+                        :action="upload_url"
+                        :show-file-list="false"
+                        :on-success="handleImgSuccess"
+                        :before-upload="beforeAvatarUpload">
+                    <el-button id="imgInput"  v-loading.fullscreen.lock="fullscreenLoading"
+                               element-loading-text="插入中,请稍候"></el-button>
+                </el-upload>
 
             </div>
+            <div class="search_item" v-if="editor == 2">
 
+                <mavon-editor :toolbars="toolbars" v-model="content"/>
+            </div>
             <el-button type="success" style="margin-top: 20px;" v-on:click="submit" :loading="loading">发布</el-button>
 
 
@@ -39,7 +54,11 @@
 <script>
     import headTop from '../components/headTop'
     import {news_edit,news_info} from '@/api/getDataEarth'
-    import { quillEditor } from 'vue-quill-editor'
+    import  { quillEditor,Quill } from 'vue-quill-editor'
+    import { ImageDrop } from 'quill-image-drop-module'
+    import ImageResize from 'quill-image-resize-module'
+    Quill.register('modules/imageDrop', ImageDrop)
+    Quill.register('modules/imageResize', ImageResize)
     export default {
         data(){
             return {
@@ -48,8 +67,23 @@
                 title:'',
                 img:'',
                 content:'',
-                editorOption:{},
-                upload_url:this.$store.state.constant.upload_url
+                editorOption:{
+                    modules:{
+                        imageDrop: true,
+                        imageResize: {
+                            displayStyles: {
+                                backgroundColor: 'black',
+                                border: 'none',
+                                color: 'white'
+                            },
+                            modules: [ 'Resize', 'DisplaySize', 'Toolbar' ]
+                        }
+                    }
+                },
+                upload_url:this.$store.state.constant.upload_url,
+                editor:1,
+                toolbars:{subfield: true},
+                fullscreenLoading:false
             }
 
         },
@@ -61,8 +95,7 @@
 
         },
         mounted(){
-
-            //console.log(this.$route.query);
+            this.$refs.myQuillEditor.quill.getModule('toolbar').addHandler('image', this.imgHandler)
         },
 
         beforeRouteEnter (to, from, next) {
@@ -80,21 +113,58 @@
         },
         methods: {
 
+            // 点击图片ICON触发事件
+            imgHandler(state) {
+                this.addRange = this.$refs.myQuillEditor.quill.getSelection()
+                if (state) {
+                    let fileInput = document.getElementById('imgInput')
+                    fileInput.click() // 加一个触发事件
+                }
+                this.uploadType = 'image'
+            },
+
             handleAvatarSuccess(res, file) {
+                this.fullscreenLoading = false
                 this.img = res.data[0];
             },
+            handleImgSuccess(e, file, fileList){
+                this.fullscreenLoading = false
+                let vm = this
+                let url = ''
+                if (this.uploadType === 'image') { // 获得文件上传后的URL地址
+                    url = e.data[0]
+                } else if (this.uploadType === 'video') {
+                    url = e.data[0]
+                }
+                if (url != null && url.length > 0) { // 将文件上传后的URL地址插入到编辑器文本中
+                    let value = url
+                    // API: https://segmentfault.com/q/1010000008951906
+                    // this.$refs.myTextEditor.quillEditor.getSelection();
+                    // 获取光标位置对象，里面有两个属性，一个是index 还有 一个length，这里要用range.index，即当前光标之前的内容长度，然后再利用 insertEmbed(length, 'image', imageUrl)，插入图片即可。
+                    vm.addRange = vm.$refs.myQuillEditor.quill.getSelection()
+                    value = value.indexOf('http') !== -1 ? value : 'http:' + value
+                    vm.$refs.myQuillEditor.quill.insertEmbed(vm.addRange !== null ? vm.addRange.index : 0, vm.uploadType, value, Quill.sources.USER) // 调用编辑器的 insertEmbed 方法，插入URL
+                } else {
+                    this.$message.error(`${vm.uploadType}插入失败`)
+                }
+                this.$refs['upload'].clearFiles() // 插入成功后清除input的内容
+            },
             beforeAvatarUpload(file) {
+                this.fullscreenLoading = true
                 const isJPG = (file.type === 'image/jpeg' || file.type === 'image/png');
                 const isLt2M = file.size / 1024 / 1024 < 1;
 
-                if (!isJPG) {
-                    this.$message.error('图片格式只支持jpg和png!');
-                }
+//                if (!isJPG) {
+//                    this.fullscreenLoading = false
+//                    this.$message.error('图片格式只支持jpg和png!');
+//                }
                 if (!isLt2M) {
+                    this.fullscreenLoading = false
                     this.$message.error('图片大小不能超过 1MB!');
                 }
-                return isJPG && isLt2M;
+                return  isLt2M;
             },
+
             onEditorChange({ editor, html, text }) {//富文本编辑器  文本改变时 设置字段值
                 this.content = html
             },
